345f78029f6870431b97482522879509e1673fbe
Mohammad Taleb <98346135+RRYR7@users.noreply.github.com>
2024-07-17 12:57:47 +0300

    Update and rename chatbot_sql.py to chatapp_sql.py

--- Tepthon/sql_helper/chatapp_sql.py
+++ Tepthon/sql_helper/chatapp_sql.py
@@ -4,7 +4,7 @@ from . import BASE, SESSION
 
 
 class ChatBot(BASE):
-    __tablename__ = "chatbot"
+    __tablename__ = "chatapp"
     chat_id = Column(String(14), primary_key=True)
     user_id = Column(String(14), primary_key=True, nullable=False)
     chat_name = Column(UnicodeText)
